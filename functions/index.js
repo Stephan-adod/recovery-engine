8e597404d93333dab02d580afb3e818d53e2e8b9
Stephan Gauglitz <stephan@adod.tech>
2025-09-21 19:52:00 +0200

    Add AT-001.md (helloWorld with timestamp)

---
+++ functions/index.js
@@ -0,0 +1,10 @@
+﻿const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.apps.length ? admin.app() : admin.initializeApp();
+
+exports.helloWorld = functions.region("europe-west1").https.onRequest(async (req, res) => {
+  const db = admin.firestore();
+  await db.collection("demo").doc("hello").set({ message: "Hello World" });
+  const doc = await db.collection("demo").doc("hello").get();
+  res.status(200).json(doc.data());
+});
